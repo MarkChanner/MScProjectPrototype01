98fd058cf001b08141f83f7b0b9e5e8a209f56f6
Mark Channer <markchanner@gmail.com>
2015-09-18 23:56:25 +0100

    Adds selectIcon method framework

--- src/Grid.java
+++ src/Grid.java
@@ -1,6 +1,6 @@
 /**
  * @author Mark Channer
- * The grid on which the game will take place
+ *         The grid on which the game will take place
  */
 public class Grid {
 
@@ -21,11 +21,23 @@ public class Grid {
     }
 
     public void displayGrid() {
-        for (FaceIcon e : emoticons) System.out.println(e + " ");
+        System.out.print("| ");
+        for (FaceIcon e : emoticons) System.out.print(e + " | ");
     }
 
     public String getEmotion(FaceIcon emoticon) {
         return emoticon.showEmotion();
     }
 
+    public void selectIcon(int icon) {
+        // If an icon has not yet been selected, just mark icon as selected.
+        // If icon an icon has already been selected, check if recently selected
+        // icon is next to previously selected one
+        // Decide if there will be a boolean 'anIconHasBeenSelected' variable in Grid
+        // or will there just be a flag inside the selected emoticon?
+    }
+
+    public void swapIcons(int a, int b) {
+
+    }
 }
