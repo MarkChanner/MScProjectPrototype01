4c13a0fd9f93c8729c08944bac77a18de6a2bcd0
Mark Channer <markchanner@gmail.com>
2015-09-19 17:42:48 +0100

    Added primitive tests of the Grid

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,19 @@
 public class Game {
 
     public static void main(String[] args) {
+        System.out.println("Game Started!!");
         Grid grid = new Grid();
-        grid.displayGrid();
+        System.out.println("Game class selecting first icon");
+        grid.selectEmoticon(0);
+        System.out.println("Game class selecting second icon");
+        grid.selectEmoticon(0);
+        System.out.println("Game class selecting a substitute icon");
+        grid.selectEmoticon(1);
+
+        System.out.println("Game class selecting first icon");
+        grid.selectEmoticon(1);
+        System.out.println("Game class selecting second icon");
+        grid.selectEmoticon(1);
+        grid.selectEmoticon(2);
     }
 }
