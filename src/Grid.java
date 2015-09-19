fbc0259dabde6c2459437ab8938a38262d792602
Mark Channer <markchanner@gmail.com>
2015-09-19 17:42:21 +0100

    Added code to check if selected emoticons are adjacent or not

--- src/Grid.java
+++ src/Grid.java
@@ -5,10 +5,15 @@
 public class Grid {
 
     private FaceIcon[] emoticons; // This will just be a single array to start with
+    private boolean firstEmoticonSelected;
+    private int emoticon1;
+
 
     public Grid() {
-        this.emoticons = new FaceIcon[6];
+        emoticons = new FaceIcon[6];
+        firstEmoticonSelected = false;
         populateGrid();
+        displayGrid();
     }
 
     public void populateGrid() {
@@ -18,23 +23,48 @@ public class Grid {
         emoticons[3] = new ConfusedFaceIcon();
         emoticons[4] = new SadFaceIcon();
         emoticons[5] = new SadFaceIcon();
+
+        emoticon1 = -1;
     }
 
     public void displayGrid() {
         System.out.print("| ");
         for (FaceIcon e : emoticons) System.out.print(e + " | ");
+        System.out.println();
     }
 
-    public String getEmotion(FaceIcon emoticon) {
+    public String getEmoticon(FaceIcon emoticon) {
         return emoticon.showEmotion();
     }
 
-    public void selectIcon(int icon) {
-        // If an icon has not yet been selected, just mark icon as selected.
-        // If icon an icon has already been selected, check if recently selected
-        // icon is next to previously selected one
-        // Decide if there will be a boolean 'anIconHasBeenSelected' variable in Grid
-        // or will there just be a flag inside the selected emoticon?
+    public void selectEmoticon(int selectedEmoticon) {
+        if (!firstEmoticonSelected) {
+            System.out.println("First Emoticon Pressed");
+            firstEmoticonSelected = true;
+            emoticon1 = selectedEmoticon;
+        } else {
+            System.out.println("Second Emoticon Pressed");
+            if (!sameEmoticonPressedTwice(selectedEmoticon)) {
+                if (selectedEmoticonsAreAdjacent(selectedEmoticon)) {
+                    System.out.println("Selected emoticons are adjacent!");
+                } else {
+                    System.out.println("Selected emoticons are NOT adjacent");
+                }
+                firstEmoticonSelected = false;
+                emoticon1 = -1;
+            } else {
+                System.out.println("Same emoticon button pushed twice. (Try a different one)"); // Loop input within this method later
+            }
+        }
+    }
+
+    private boolean sameEmoticonPressedTwice(int selectedEmoticon) {
+        return (emoticon1 == selectedEmoticon);
+    }
+
+    private boolean selectedEmoticonsAreAdjacent(int selectedEmoticon) {
+        if ((emoticon1 + 1) == selectedEmoticon || (emoticon1 - 1) == selectedEmoticon) return true;
+        return false;
     }
 
     public void swapIcons(int a, int b) {
