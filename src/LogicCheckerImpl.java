510a2453e6e0460edded6347147f3e7a310a35bd
Mark Channer <markchanner@gmail.com>
2015-09-22 00:48:35 +0100

    "Removed code from check() and spread to getMatches()"

--- src/LogicCheckerImpl.java
+++ src/LogicCheckerImpl.java
@@ -9,14 +9,22 @@ public class LogicCheckerImpl implements LogicChecker {
     private Grid grid;
 
     /**
-     * So far only check for horizontal matches
+     * Not yet finished. Doesn't work for horizontal
      */
     public List<Tile> check(Grid grid) {
-        List<Tile> matchingTiles = new ArrayList<>();
         int rows = grid.getRows();
         int cols = grid.getCols();
         Tile[][] tiles = grid.getAllTiles();
+        List<Tile> matchingRows = getMatches(tiles, rows, cols);
+        List<Tile> matchingCols = getMatches(tiles, cols, rows);
+        List<Tile> mergedList = new ArrayList<>();
+        mergedList.addAll(matchingRows);
+        mergedList.addAll(matchingCols);
+        return mergedList;
+    }
 
+    public List<Tile> getMatches(Tile[][] tiles, int rows, int cols) {
+        List<Tile> matchingTiles = new ArrayList<>();
         for (int i = (rows - 1); (i >= 0); i--) {
             for (int j = 0; (j < cols - 1); j++) {
                 if (tiles[i][j].getEmotion().equals(tiles[i][j + 1].getEmotion())) {
