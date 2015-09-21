8351f6ad0c5f09d1bedbc8ac18942fed30556394
Mark Channer <markchanner@gmail.com>
2015-09-21 21:14:12 +0100

    Implements methods and makes class abstract.

--- src/AbstractTile.java
+++ src/AbstractTile.java
@@ -1,5 +1,24 @@
 /**
- * Created by Mark on 21/09/2015.
+ * @author Mark Channer
+ *
+ * AbstractTile holds a reference to a GamePiece, which
+ * it can call methods on
  */
-public class AbstractTile {
+public abstract class AbstractTile implements Tile {
+
+    private GamePiece emoticon;
+
+    public AbstractTile(GamePiece emoticon) {
+        this.emoticon = emoticon;
+    }
+
+    @Override
+    public String getFace() {
+        return emoticon.showFace();
+    }
+
+    @Override
+    public void setFace(GamePiece gp) {
+        this.emoticon = gp;
+    }
 }
