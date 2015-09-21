5822ed19d9138cec5e4bd4e796753d0b504b009b
Mark Channer <markchanner@gmail.com>
2015-09-21 13:58:08 +0100

    Deleting tests of Grid that applied to the single array Grid

--- src/Game.java
+++ src/Game.java
@@ -4,35 +4,7 @@
 public class Game {
 
     public static void main(String[] args) {
-        System.out.println("Constructing Grid1");
+        System.out.println();
         Grid grid1 = new Grid();
-        System.out.println("Grid1 selecting first icon");
-        grid1.selectEmoticon(1);
-        System.out.println("Grid1 selecting second icon");
-        grid1.selectEmoticon(1);
-        System.out.println("Grid1 selecting a substitute icon");
-        grid1.selectEmoticon(2);
-        System.out.println();
-
-        System.out.println("Grid1 selecting first icon");
-        grid1.selectEmoticon(3);
-        System.out.println("Grid1 selecting second icon");
-        grid1.selectEmoticon(4);
-        System.out.println();
-
-        System.out.println("Constructing Grid2");
-        Grid grid2 = new Grid();
-        System.out.println("Grid2 selecting first icon");
-        grid2.selectEmoticon(0);
-        System.out.println("Grid2 selecting second icon");
-        grid2.selectEmoticon(1);
-        System.out.println();
-
-        System.out.println("Constructing Grid3");
-        Grid grid3 = new Grid();
-        System.out.println("Grid3 selecting first icon");
-        grid3.selectEmoticon(5);
-        System.out.println("Grid3 selecting second icon");
-        grid3.selectEmoticon(4);
     }
 }
