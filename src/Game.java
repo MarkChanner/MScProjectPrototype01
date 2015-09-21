ca7c0e817376ce38f7729c0ac463174c11740420
Mark Channer <markchanner@gmail.com>
2015-09-21 16:00:46 +0100

    Added new tests for the swapping of icons in 2D array

--- src/Game.java
+++ src/Game.java
@@ -3,8 +3,39 @@
  */
 public class Game {
 
+    private Grid grid1;
+
+    public Game() {
+        this.grid1 = new Grid();
+    }
+
     public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+
+    public void start() {
         System.out.println();
-        Grid grid1 = new Grid();
+        grid1.populateGrid();
+        grid1.displayGrid();
+        System.out.println();
+        /*grid1.selectEmoticon(6,0);
+        grid1.selectEmoticon(6,0);
+        grid1.selectEmoticon(6,1);
+        grid1.selectEmoticon(3,3);
+        grid1.selectEmoticon(3,4);*/
+
+        grid1.selectEmoticon(4,0);
+        grid1.selectEmoticon(4,3);
+        grid1.selectEmoticon(4,4);
+
+
+
+
+
+
+
+        //grid1.exchangeEmoticon(6,0, new SadFaceIcon());
+        //grid1.displayGrid();
     }
 }
