9e2f8d85d3427fe4933b4fd6982e4cbfb374032f
Mark Channer <markchanner@gmail.com>
2015-09-24 16:35:16 +0100

    Adds testRetrieveGamePieceAndShowType()

---
+++ src/test/java/gamepieces/HappyGamePieceTest.java
@@ -0,0 +1,18 @@
+package gamepieces;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Mark Channer
+ */
+public class HappyGamePieceTest {
+
+    @Test
+    public void testRetrieveGamePieceAndShowType() throws Exception {
+        GamePiece gp = new HappyGamePiece("H");
+        GamePiece retrievedPiece = gp.retrieveGamePiece();
+        assertEquals("H", retrievedPiece.showType());
+    }
+}
