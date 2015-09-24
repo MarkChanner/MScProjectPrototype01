0794d2065cb60b2ae8cc50b8eae5e924166feb13
Mark Channer <markchanner@gmail.com>
2015-09-24 23:36:21 +0100

    Adds coordinates and setter/getter methods

--- src/main/java/gamepieces/AbstractGamePiece.java
+++ src/main/java/gamepieces/AbstractGamePiece.java
@@ -1,13 +1,29 @@
 package gamepieces;
+
 /**
  * @author Mark Channer
  */
 public abstract class AbstractGamePiece implements GamePiece {
 
     private String type;
+    private int[] coordinates;
 
-    public AbstractGamePiece(String type) {
-        this.type = type;
+    public AbstractGamePiece(String t) {
+        type = t;
+        coordinates = new int[2];
+        coordinates[0] = -1;
+        coordinates[1] = -1;
+    }
+
+    @Override
+    public void setCoordinates(int row, int column) {
+        coordinates[0] = row;
+        coordinates[1] = column;
+    }
+
+    @Override
+    public int[] getCoordinates() {
+        return coordinates;
     }
 
     public abstract GamePiece retrieveGamePiece();
