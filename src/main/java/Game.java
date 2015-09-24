df69d791063c949a1c6ca353e60a1fe6cf68d4a0
Mark Channer <markchanner@gmail.com>
2015-09-24 16:23:37 +0100

    Changes Board variable name to board

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,11 +7,11 @@ import gameboard.BoardImpl;
  */
 public class Game {
 
-    private Board grid01;
+    private Board board;
     public static final int SIZE = 7;
 
     public Game() {
-        this.grid01 = new BoardImpl(SIZE);
+        this.board = new BoardImpl(SIZE);
     }
 
     public static void main(String[] args) {
@@ -21,18 +21,18 @@ public class Game {
 
     public void start() {
         System.out.println();
-        grid01.populateBoard();
-        grid01.displayBoard();
+        board.populateBoard();
+        board.displayBoard();
         System.out.println();
-        grid01.selectTile(6, 0);
-        grid01.selectTile(6, 0);
-        grid01.selectTile(6, 1);
-        grid01.selectTile(3, 3);
-        grid01.selectTile(3, 4);
+        board.selectTile(6, 0);
+        board.selectTile(6, 0);
+        board.selectTile(6, 1);
+        board.selectTile(3, 3);
+        board.selectTile(3, 4);
 
-        //grid01.selectTile(4,0);
-        //grid01.selectTile(4,3);
-        //grid01.selectTile(4,4);
+        //board.selectTile(4,0);
+        //board.selectTile(4,3);
+        //board.selectTile(4,4);
 
     }
 }
