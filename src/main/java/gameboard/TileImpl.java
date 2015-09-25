a93131c8d505b2be9a2e24af85a203936abd51bc
Mark Channer <markchanner@gmail.com>
2015-09-25 20:34:53 +0100

    Makes TileImpl implement Tile, rather than extend AbstractTile which has not been removed from the project

--- src/main/java/gameboard/TileImpl.java
+++ src/main/java/gameboard/TileImpl.java
@@ -6,10 +6,45 @@ import gamepieces.GamePiece;
  * @author Mark Channer
  * ileImpl extends AbstractTile
  */
-public class TileImpl extends AbstractTile {
+public class TileImpl implements Tile {
 
-    public TileImpl(int row, int col, GamePiece gp) {
+    /*public TileImpl(int row, int col, GamePiece gp) {
         super(row, col, gp);
+    }*/
+
+    private int row;
+    private int column;
+    private GamePiece piece;
+
+    public TileImpl(int r, int c, GamePiece gp) {
+        row = r;
+        column = c;
+        piece = gp;
+        piece.setCoordinates(row, column);
+    }
+
+    @Override
+    public int[] getCoordinates() {
+        int[] location = new int[2];
+        location[0] = row;
+        location[1] = column;
+        return location;
+    }
+
+    @Override
+    public GamePiece getGamePiece() {
+        return piece.retrieveGamePiece();
+    }
+
+    @Override
+    public void setGamePiece(GamePiece gp) {
+        piece = gp;
+        piece.setCoordinates(row, column);
+    }
+
+    @Override
+    public String getPieceType() {
+        return piece.showType();
     }
 
 }
