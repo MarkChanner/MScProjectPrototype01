5ffedbb25384004645cabada5691e553b6f782f8
Mark Channer <markchanner@gmail.com>
2015-09-30 22:05:40 +0100

    Added JavaDocs

--- src/main/java/gameboard/Tile.java
+++ src/main/java/gameboard/Tile.java
@@ -5,18 +5,45 @@ import gamepieces.GamePiece;
 /**
  * @author Mark Channer
  *
- * Tile represents each square on the grid
+ * Each Tile represents a square on the board. It provides methods for
+ * accessing the location of the Tile on the board, and for retrieving
+ * both the type of GamePiece occupying it and the type GamePiece it is
  */
 public interface Tile {
 
+    /**
+     * Returns the row at the specified position in the array
+     *
+     * @return the row location of this tile
+     */
     int getRow();
 
+    /**
+     * Returns the column at the specified position in the array
+     *
+     * @return the column location of the Tile
+     */
     int getColumn();
 
+    /**
+     * Returns the Game Piece occupying the Tile
+     *
+     * @return the Game Piece occupying the Tile
+     */
     GamePiece getGamePiece();
 
+    /**
+     * Sets a GamePiece to occupy the Tile
+     *
+     * @param gp the GamePiece to occupy the Tile
+     */
     void setGamePiece(GamePiece gp);
 
+    /**
+     * Returns a String representing the type of the game piece
+     *
+     * @return the game piece type
+     */
     String getPieceType();
 
 }
