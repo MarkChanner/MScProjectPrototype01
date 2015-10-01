8093ab950d60563a7b37dbdf50627b5c15958661
Mark Channer <markchanner@gmail.com>
2015-10-01 09:26:32 +0100

    Adds JavaDocs

--- src/main/java/gameboard/BoardPopulator.java
+++ src/main/java/gameboard/BoardPopulator.java
@@ -3,14 +3,27 @@ package gameboard;
 import gamepieces.GamePiece;
 
 /**
- * @author Mark Channer
- * BoardPopulator interface allows different game pieces to populate the Board.
- * Useful for creating a non-randomly generated board for testing
+ * Implementations of this interface are used to populate the Board with game pieces.
+ * For testing purposes, an implementation can populate the board with a set layout,
+ * rather than at random, which would make testing difficult.
+ *
+ * @author Mark Channer for first prototype of Birkbeck MSc Computer Science final project
+ *
  */
 public interface BoardPopulator {
 
+    /**
+     * Populates the given Board with game pieces
+     *
+     * @param board element to be populated with game pieces
+     */
     void populate(Board board);
 
+    /**
+     * Creates a game piece
+     *
+     * @return a game piece
+     */
     GamePiece generateGamePiece();
 
 }
