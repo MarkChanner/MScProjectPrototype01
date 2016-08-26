c5c6b90052b7916f20fea5c7550ad53ff7b5354b
markchanner <markchanner@gmail.com>
2016-08-26 12:49:05 +0100

    Adds comments

--- src/main/java/model/gameboard/Selections.java
+++ src/main/java/model/gameboard/Selections.java
@@ -5,22 +5,62 @@ package model.gameboard;
  */
 public interface Selections {
 
+    /**
+     * Sets selections to value of -1
+     */
     void reset();
 
+    /**
+     * Returns the user's first selection if it has been made
+     *
+     * @return an int[] array containing x and y values of first selection
+     */
     int[] getSelection01();
 
+    /**
+     * Sets x and y values for the user's first selection
+     *
+     * @param x the x value to set for first selection
+     * @param y the y value to set for first selection
+     */
     void setSelection01(int x, int y);
 
+    /**
+     * @return true if user's has made first selection
+     */
     boolean selection01AlreadyMade();
 
+    /**
+     * Returns the user's second selection if it has been made
+     *
+     * @return an int[] array containing x  y values of second selection
+     */
     int[] getSelection02();
 
+    /**
+     * Sets x and y values for the user's second selection
+     *
+     * @param x the x value to set for second selection
+     * @param y the y value to set for second selection
+     */
     void setSelection02(int x, int y);
 
+    /**
+     * @return true if the x and y values of each selection are the same
+     */
     boolean sameSelectionMadeTwice();
 
+    /**
+     * @return true if the second selection is NOT directly vertically
+     * or horizontally adjacent to the first selection
+     */
     boolean areNotAdjacent();
 
+    /**
+     * Makes the user's second selection become the user's first selection.
+     * This method is called when the user's second selection is not directly
+     * vertically or horizontally adjacent to the first selection
+     */
     void secondSelectionBecomesFirstSelection();
 
 }
--- src/main/java/model/gameboard/SelectionsImpl.java
+++ src/main/java/model/gameboard/SelectionsImpl.java
@@ -15,20 +15,29 @@ public class SelectionsImpl implements Selections {
         reset();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void reset() {
-        selection01[X] = -1;
-        selection01[Y] = -1;
+        selection01[X] = -2;
+        selection01[Y] = -2;
         selection02[X] = -1;
         selection02[Y] = -1;
         selection01AlreadyBeenMade = false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int[] getSelection01() {
         return selection01;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setSelection01(int x, int y) {
         selection01[X] = x;
@@ -36,27 +45,42 @@ public class SelectionsImpl implements Selections {
         selection01AlreadyBeenMade = true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean selection01AlreadyMade() {
         return selection01AlreadyBeenMade;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int[] getSelection02() {
         return selection02;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setSelection02(int x, int y) {
         selection02[X] = x;
         selection02[Y] = y;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean sameSelectionMadeTwice() {
         return (selection01[X] == selection02[X] && selection01[Y] == selection02[Y]);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean areNotAdjacent() {
         if ((selection01[X] == selection02[X]) &&
@@ -69,6 +93,9 @@ public class SelectionsImpl implements Selections {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void secondSelectionBecomesFirstSelection() {
         selection01[X] = selection02[X];
